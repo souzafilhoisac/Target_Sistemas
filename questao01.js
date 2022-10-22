682fa2700808ea31c550281c1887d1874e6aef7a
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-10-22 12:46:05 -0300

    finalizando os exercícios 1 e 2

--- questao01.js
+++ questao01.js
@@ -1,3 +1,17 @@
+/*1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?*/
+
 let soma = 0;
 let k = 0;
 
--- questao02.js
+++ questao02.js
@@ -1,7 +1,13 @@
+/* 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+IMPORTANTE:
+Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+ */
+
 let num1 = 0;
 let num2 = 1;
 let num3 = 0;
-let seq = [0, 1];
+let seq = [0];
 
 function Fibonacci(num){
 
